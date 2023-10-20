b0a452f716bfd10609952ca3285ba7b9e57144d3
jcbhmr <jcbhmr@outlook.com>
2023-10-20 11:41:23 -0500

    use new badges as pseudo-names and DONT actually rename

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
-import { Converter } from "typedoc";
+import { Converter, ReflectionKind } from 'typedoc';
 
-/**
- * @param {Readonly<import('typedoc').Application>} app
- */
+/** @param {Readonly<import('typedoc').Application>} app */
 export function load(app) {
   /**
    * @param {import('typedoc').Context} context
    * @param {import('typedoc').DeclarationReflection} reflection
    */
   function handleCreateDeclaration(context, reflection) {
-    if (reflection.name !== "default" && reflection.name !== "export=") {
-      return;
-    }
+    if (['default', 'export='].includes(reflection.name) && reflection.parent) {
+      reflection.kind = ReflectionKind.Property;
 
-    // reflection.escapedName is the cheapest option
-    if (
-      reflection.escapedName &&
-      reflection.escapedName !== "default" &&
-      reflection.name !== "export="
-    ) {
-      reflection.name = reflection.escapedName;
-      return;
-    }
+      add_badge: {
+        const symbol = context.project.getSymbolFromReflection(reflection);
+        if (symbol.declarations?.length) {
+          const node = symbol.declarations[0];
+          /** @type {{ getText(): string } | undefined} */
+          const name = node.name;
+          if (name) {
+            reflection.flags.unshift(node.name.getText());
+            break add_badge;
+          }
+        }
 
-    // if that does not work, try harder
-    const symbol = context.project.getSymbolFromReflection(reflection);
-    if (symbol && symbol.declarations && symbol.declarations[0]) {
-      /** @type {any} */
-      const node = symbol.declarations[0];
-      if (node && node.name) {
-        reflection.name = node.name.getText();
-        return;
-      }
-    }
-
-    // Finally, fallback to the file name
-    if (reflection.parent && reflection.parent.name) {
-      // Removes the folder name
-      const name = reflection.parent.name.split("/").pop();
-      if (name) {
-        // Example: User.entity becomes just User
-        reflection.name = name.split(".")[0];
+        let name = reflection.parent.getFriendlyFullName();
+        name = name.split('/').at(-1);
+        name = name.replace(/[_.\- ]+(\w)/g, (m, x) => x.toUpperCase());
+        reflection.flags.unshift(name);
       }
     }
   }
-
   app.converter.on(Converter.EVENT_CREATE_DECLARATION, handleCreateDeclaration);
 }
